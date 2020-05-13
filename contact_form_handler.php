746c2663d320e2c2d92aeb2bd24bdcf0023cf3ce
Divyanshu <35335337+sdiv0@users.noreply.github.com>
2020-05-13 09:28:43 +0530

    Added subject line for the receiver of email

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -1,16 +1,17 @@
 <?php
 	$name = $_POST['name'];
 	$visitor_email = $_POST['email'];
+	$subject = $_POST['subject']
 	$message = $_POST['message'];
 
-	$email_from = 'sdiv0.github.io;
+	$email_from = 'sdiv0.github.io/hmi;
 
-	$email_subject = 'Website New Contact Form Submission';
+	$email_subject = "Subject: $subject.\n";
 	$email_body = "User Name: $name.\n".
 					"User Email: $visitor_email.\n ".
 					"Message: $message.\n";
 
-	$to = "divyanshu17048@iiitd.ac.in";
+	$to = "hmi@iiitd.ac.in";
 
 	$headers = "From $email_from \r\n";
 	$headers = "Reply-To: $visitor_email \r\n";
