6d1f55a17b436c20842ffbe5e8281366e7321ae4
Divyanshu <35335337+sdiv0@users.noreply.github.com>
2020-07-26 22:54:10 +0530

    trying to fix multiple page issue

--- js/main.js
+++ js/main.js
@@ -217,12 +217,12 @@ jQuery(document).ready(function($) {
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
       e.preventDefault();
 
-      var hash = this.hash;
+      var hash = e.currentTarget.getAttribute('href');
 
       $('html, body').animate({
         'scrollTop': $(hash).offset().top
       }, 600, 'easeInOutCirc', function(){
-        window.location.hash = hash;
+        window.location = "https://sdiv0.github.io/hmi/"+hash;
       });
 
     });
